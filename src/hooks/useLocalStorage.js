35692edf55f128f980e37b0d46a155e99a5ee8c5
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-03 21:59:11 -0500

    useLocalStorage hook set up

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+
+const useLocalStorage = (key, initialValue) => {
+
+  //To retrieve an item from localStorage, call localStorage.getItem('itemName')
+//If that item doesn't exist, it will return undefined
+  const [storedValue, setStoredValue] = useState(() =>{
+
+    //get from localStorage by key
+    const item= window.localStorage.getItem(key);
+
+    //Parse and return stored json or, if undefined, return initialValue
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  //setter function to setValue
+
+  const setValue = value => {
+    // Save State
+    setStoredValue(value);
+
+    //Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+//return the variables in useState as an array
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage
+
+
+
